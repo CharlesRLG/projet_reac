a8fad09aa32e8ff61ce30209b1d85db3a8b69e92
charles <charlesrabeclegloahec@gmail.com>
2022-03-11 13:37:47 +0100

    mise à jour color picker

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './App.css'
 import ProductList from './components/productslist.js'
 import Cart from './components/cart.js'
 import FooterPerso from './components/footerPerso.js'
+import ColorPickerMenu from './components/color-picker-menu.js';
 
 function App() {
+
   return (
     <div className="App">
       < Headerperso />
+      < ColorPickerMenu />
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
+
         < ProductList />
         < Cart />
       </header>
--- src/components/footerPerso.js
+++ src/components/footerPerso.js
@@ -1,6 +1,6 @@
 function footerPerso() {
     return (
-      <div className="footerrperso">
+      <div className="footerperso">
         <footer className="App-footerperso">
           
           <h1>footer footer footer</h1>
