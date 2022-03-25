150133ae31843006db50937616315630d787eb8e
charles <charlesrabeclegloahec@gmail.com>
2022-03-25 11:25:31 +0100

    ajout du onClick ainsi que de la fonction handleClick

--- src/components/objectList.js
+++ src/components/objectList.js
@@ -4,14 +4,18 @@ import cv1 from '../assets/img/cv_1.png'
 import cv2 from '../assets/img/cv_2.png'
 import cv3 from '../assets/img/cv_3.png'
 
-
-
-
 let objectsList = [
 {name : "CV1",description : "Curriculum 1",price : "5€",image : cv1},
 {name : "CV2",description : "Curriculum 2",price : "17€",image : cv2},
 {name : "CV3",description : "Curriculum 3",price : "3827€",image : cv3},
 ]       
+
+
+  function handleClick() {
+    alert(objectsList.description);
+  }
+
+
 
 
 export default function objectListFunc() {
@@ -20,7 +24,7 @@ export default function objectListFunc() {
       {objectsList.map(({name, description, price, image})=>(
       <ul>
           <li>{name}</li> 
-          <a href={description}><img src={image} class="imageCv"/></a>
+          <a href={description} onClick={handleClick}><img src={image} class="imageCv"/></a>
           {/* <li>{alert(description)}</li> */}
           <li>{price}</li>
       </ul>
@@ -28,3 +32,5 @@ export default function objectListFunc() {
     </ul>
   )
 }
+
+
